d86438a22f74c905d43279dd7e2caec1fa411d3e
Johary Enrique Fuentes Varguez <38574115+Jerandren@users.noreply.github.com>
2021-02-03 19:07:08 -0600

    dihuwe

--- Gobierno/firstWeb/firstWeb/settings.py
+++ Gobierno/firstWeb/firstWeb/settings.py
@@ -119,10 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os
-STATIC_URL = '/static/'
-BASE_DIR = os.path.abspath(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
