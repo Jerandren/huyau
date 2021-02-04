22af20fdc697a05b501b647be239bb03b66905f8
Johary Enrique Fuentes Varguez <38574115+Jerandren@users.noreply.github.com>
2021-02-03 18:41:39 -0600

    50.2

--- Gobierno/firstWeb/firstWeb/settings.py
+++ Gobierno/firstWeb/firstWeb/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'firstWeb.wsgi.application'
 
 import dj_database_url
 from decouple import config
+
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
