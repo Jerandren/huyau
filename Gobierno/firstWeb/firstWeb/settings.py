3f6b677b7c63c36fac062d439944bc75a4630dc9
Johary Enrique Fuentes Varguez <38574115+Jerandren@users.noreply.github.com>
2021-02-03 18:56:17 -0600

    import os

--- Gobierno/firstWeb/firstWeb/settings.py
+++ Gobierno/firstWeb/firstWeb/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+import os
 STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
