239812196a2a8f6b7f174341068d15dd06545dcc
Johary Enrique Fuentes Varguez <38574115+Jerandren@users.noreply.github.com>
2021-02-03 18:58:30 -0600

    settings

--- Gobierno/firstWeb/firstWeb/settings.py
+++ Gobierno/firstWeb/firstWeb/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'firstWeb.urls'
@@ -125,3 +126,4 @@ STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
